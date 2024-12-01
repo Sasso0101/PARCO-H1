ede7c2bf329266bdc06f9a690a5aeca877eefed9
sasso0101 <git@sasso.dev>
2024-12-01 09:50:51 +0100

    Fix

--- peak_bandwidth.c
+++ peak_bandwidth.c
@@ -5,12 +5,11 @@
 int main(int argc, char **argv) {
   int size = 4096;
   if (argc > 1) {
-      size = strtol(argv[1], NULL, 10);
+    size = strtol(argv[1], NULL, 10);
   }
 
   float *a;
   double start, end;
-  int i;
 
   // Allocate memory for array
   a = (float *)malloc(size * sizeof(float));
@@ -18,10 +17,10 @@ int main(int argc, char **argv) {
   // Write some values to the array
   start = omp_get_wtime();
   #pragma omp parallel for simd
-  for (i = 0; i < ARRAY_SIZE; i++) {
+  for (int i = 0; i < ARRAY_SIZE; i++) {
     a[i] = 42.0;
   }
-  end_time = omp_get_wtime();
+  end = omp_get_wtime();
 
   // Calculate and print the bandwidth
   double bandwidth = (size * sizeof(float)) / (end - start) / 1e9;
